efc7939c0b8a1b4188a73aa76dff91cf0f0d8c95
Will Puricz <wpuricz@kpmg.com>
2018-06-07 13:56:58 -0400

    Fixed upload of image

--- src/js/components/Hello.vue
+++ src/js/components/Hello.vue
@@ -2,19 +2,36 @@
   <div class="hello">
     <h3>{{ msg }}</h3>
     <!-- <input type="file" id="files" ref="files" multiple @change="handleFilesUpload()"/> -->
-    <!-- <input type="file" @change="loadTextFromFile"/> -->
-    <input type="text" name="ipfstext" v-model="ipfstext"/>
+    <input type="file" @change="loadTextFromFile"/>
     <button @click="submitFiles()">Submit</button>
-    <button @click="display()">Display Text</button>
-    <div>Saved Text: {{ipfsSavedText}}</div>
+    
+    <div>
+      <!-- <input type="text" name="ipfstext" v-model="ipfstext"/>
+      <button @click="submitText()">Submit</button> -->
+      <!-- <button @click="display()">Display Text</button> -->
+      <!-- <div>Saved Text: {{ipfsSavedText}}</div> -->
+    </div>
   </div>
 </template>
 
 <script>
+// EOS Initialization and Config
+import * as Eos from 'eosjs';
+import secrets from '../../../config/secrets'
+
+let config = {
+  keyProvider:secrets.keys,
+  httpEndpoint: secrets.endpoint
+};
+let eos = Eos(config); // 127.0.0.1:8888
+let appcode = 'ipfs.app';
+
+// IPFS Initialization
 const IPFS = require('ipfs-api');
 const ipfs = new IPFS({ host: "ipfs.infura.io", port: 5001, protocol: "https" });
 //https://alligator.io/vuejs/file-reader-component/
 //https://forums.asp.net/t/1977558.aspx?HTML5+fileReader+onloadend+event+not+getting+fire+with+while+loop+loop
+
 export default {
   name: 'hello',
   data() {
@@ -30,8 +47,18 @@ export default {
       const file = ev.target.files[0];
       const reader = new FileReader();
 
-      reader.onload = e => this.$emit("load", e.target.result);
-      reader.readAsText(file);
+      //reader.onload = e => this.$emit("load", e.target.result);
+      //reader.onload = e => console.log(e.target.result);
+      //reader.onload = e => console.log(e.target.result);
+      //reader.readAsText(file);
+      let hash = "QmQ9UsE5S68bDZn4ne8xu55qMTjioL5bfvQmdHcT65Hvsv"
+      reader.onload = async () => {
+        var result = await ipfs.add([Buffer.from(reader.result)]);
+        console.log(result[0].hash);
+        console.log("finished on load");
+      };
+      reader.readAsArrayBuffer(file);
+
     },
     handleFilesUpload() {
       let uploadedFiles = this.$refs.files.files;
@@ -52,12 +79,13 @@ export default {
       //   this.files.push( uploadedFiles[i] );
       // }
     },
-    saveToIpfs(reader) {
+    saveFileToIpfs(reader) {
+
       console.log("entered save to ipfs");
       let ipfsId
-    const buffer = Buffer.from(reader.result)
-    console.log("calling ipfs");
-    ipfs.add(buffer, { progress: (prog) => console.log(`received: ${prog}`) })
+      const buffer = Buffer.from(reader.result)
+      console.log("calling ipfs");
+      ipfs.add(buffer, { progress: (prog) => console.log(`received: ${prog}`) })
       .then((response) => {
         console.log(response)
         ipfsId = response[0].hash
@@ -67,34 +95,76 @@ export default {
       }).catch((err) => {
         console.error(err)
       })
+
     },
-    submitFiles() {
+    async submitText() {
+
       if(this.ipfstext === "") {
         console.log("text is blank");
         return;
       }
       var buf = Buffer.from(this.ipfstext, 'utf8');
-      ipfs.add(buf, (err, res) => {
-        if(err) {
-          console.log("error saving to ipfs: " + err);
-        }
+      
+      try {
+          let response = await ipfs.add(buf)
+          let hash = response[0].hash;
+          console.log("Hash from IPFS: " + hash);
+          await this.saveHashToBlockchain(hash,"");
+      }catch(e) {
+        console.log("error saving to ipfs and blockchain: " + e);
+      }
 
-        res.forEach((item) => {
-          if (item && item.hash) {
-            console.log('successfully stored', item.hash)
-            //display(item.hash)
-          }
-        })
-      })
     },
-    display() {
+    async display() {
       // buffer: true results in the returned result being a buffer rather than a stream
-      let hash = "QmPnnvFkvSCmPvMdMtSNbXrFWQX5XYc4YkcVU4cujyyH8Z";
+      let rows = await this.getHashFromBlockchain();
+      let hash = rows[0].text_hash;
+      //let hash = "QmPnnvFkvSCmPvMdMtSNbXrFWQX5XYc4YkcVU4cujyyH8Z";
       ipfs.cat(hash, (err, data) => {
         if (err) { return console.error('ipfs cat error', err) }
         this.ipfsSavedText = data.toString('utf8');
         
       })
+    },
+    async getHashFromBlockchain() {
+      let tableQuery = {
+        "json":true,
+        "scope":appcode,
+        "code":appcode,
+        "table":"ipfsdata"
+      }
+      
+      let result = await eos.getTableRows(tableQuery);
+      console.log(result.rows);
+      return result.rows;
+    },
+    async saveHashToBlockchain(textHash,imageHash) {
+      try {
+        this.processing = true;
+        let response = await eos.transaction({
+          actions: [
+            {
+              account: appcode,
+              name: 'savehash',
+              authorization: [{
+                actor: "dummy",
+                permission: 'active'
+              }],
+              data: {
+                text_hash:textHash,
+                image_hash:imageHash
+              }
+            }
+          ]
+        })
+        console.log(response)
+        return response;
+        //this.processing = false;
+
+      }catch(e) {
+        alert(JSON.parse(e).error.what)
+        
+      }
     }
     
     
